0ced072e806770a6c3cccb67e7481bd65800ace2
Benjamin Jones <bljones36@gmail.com>
2018-11-03 03:16:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,13 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  ""
+  "SELECT users.name, SUM(pledges.amount)
+  FROM users
+  INNER JOIN pledges
+  ON users.id = pledges.user_id
+  WHERE users.category = music
+  GROUP BY users.name
+  ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
