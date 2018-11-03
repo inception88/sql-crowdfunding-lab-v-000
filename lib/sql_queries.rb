3be6d44b70bf3f8961fd8f0a72b8ec7681a32f49
Benjamin Jones <bljones36@gmail.com>
2018-11-03 03:24:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM users
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON users.id = pledges.user_id
   JOIN projects
   ON projects.category = 'music'
