98dcad219597ee9037ff63e04df447fa6e90b967
Benjamin Jones <bljones36@gmail.com>
2018-11-03 03:26:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,8 +48,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   JOIN pledges
   ON projects.id = pledges.projects_id
-  JOIN projects
-  ON projects.category = 'music'
+  WHERE project.category = 'music'
   GROUP BY pledges.amount"
 end
 
