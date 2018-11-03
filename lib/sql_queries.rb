526b788d4049b9c561ad92ebe025d68a9ebf8fa0
Benjamin Jones <bljones36@gmail.com>
2018-11-03 03:16:33 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,8 +49,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   LEFT JOIN pledges
   ON users.id = pledges.user_id
   WHERE users.category = music
-  GROUP BY users.name
-  ORDER BY SUM(pledges.amount)"
+  GROUP BY users.name"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
