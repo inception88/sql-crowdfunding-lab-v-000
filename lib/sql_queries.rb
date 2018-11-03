028d9948053656ff188ce12e551ae6111eb16bd7
Benjamin Jones <bljones36@gmail.com>
2018-11-03 03:21:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   LEFT JOIN pledges
   ON users.id = pledges.user_id
   JOIN projects
-  ON projects.category = 'music'"
+  ON projects.category = 'music'
+  GROUP BY pledges.amount"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
